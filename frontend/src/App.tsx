fff1881c621a674bce0ca20d1392076093ac4322
manojitht <manojithtjmkm@gmail.com>
2026-03-18 22:34:24 +0530

    feat: signup login dashboard ui implemented

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/signup" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
